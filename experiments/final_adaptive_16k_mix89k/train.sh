585de6a7de40df51656678ebf7cb5a982930e5f9
CSfufu <3220105511cs@gmail.com>
2025-08-13 14:29:18 +0800

    experiment

---
+++ experiments/final_adaptive_16k_mix89k/train.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -x
+
+
+export PYTHONUNBUFFERED=1
+# MODEL_PATH=Qwen/Qwen2.5-VL-3B-Instruct  # replace it with your local file path
+export WANDB_BASE_URL=https://api.wandb.ai
+
+
+# lennonye的api key
+export WANDB_API_KEY=5b12869314286239ee1d4f6888a9c2c767e24ef5
+
+# fufu的api key
+# export WANDB_API_KEY="9c62933f71d04da55b4bc9e7f61f72356dfbf071"
+PROJECT_NAME=AdaEntropy
+RUN_ID=final_adaptive_16k_mix89k
+
+export PYTHONUNBUFFERED=1
+
+EXPERIMENT_NAME=final_adaptive_16k_mix89k
+MODEL_PATH=/cpfs/user/yym/models/adaptive-mm-v02  # replace it with your local file path
+#MODEL_PATH=/cpfs/data/chenshuang/models/Qwen2.5-VL-7B-Instruct # replace it with your local file path
+
+python3 -m verl.trainer.main \
+    config=/cpfs/user/yym/projects/Adaptive-VL-worktrees/exp_crucial_token_entropy/experiments/${EXPERIMENT_NAME}/config.yaml \
+    data.train_files=/cpfs/user/yym/hugging_face/datasets/mix_geo3k_like/mix_geo3k_like_w_global_id.parquet \
+    data.val_files=/cpfs/user/yym/hugging_face/datasets/mathvista_in_geo3k_format/mathvista_in_geo3k_format_w_global_id.parquet \
+    worker.actor.model.model_path=${MODEL_PATH} \
+    worker.actor.micro_batch_size_per_device_for_update=1 \
+    worker.actor.micro_batch_size_per_device_for_experience=8 \
+    trainer.experiment_name=${EXPERIMENT_NAME} \
+    trainer.load_checkpoint_path=/cpfs/user/yym/projects/Adaptive-VL-worktrees/exp_crucial_token_entropy/checkpoints/${PROJECT_NAME}/${EXPERIMENT_NAME} \
+    trainer.n_gpus_per_node=8 \
+    trainer.nnodes=8 \
+    trainer.project_name=${PROJECT_NAME} \
+    trainer.wandb_run_id=${RUN_ID} \
+    trainer.val_freq=5 \
+    worker.actor.fsdp.torch_dtype=bf16 \
+    worker.actor.optim.strategy=adamw_bf16 \
+    # trainer.load_checkpoint_path=/cpfs/user/yym/projects/Adaptive-VL-worktrees/exp_crucial_token_entropy/checkpoints/${PROJECT_NAME}/${EXPERIMENT_NAME} \
+    # worker.actor.micro_batch_size_per_device_for_update=1 \
+    # worker.actor.micro_batch_size_per_device_for_experience=8 \
+    # worker.rollout.tensor_parallel_size=1 \
